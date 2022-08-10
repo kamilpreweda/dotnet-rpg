47310e6c65e508dfadc4287e58d4b6e759a0298d
Kamil Preweda <preweda.kamil@o2.pl>
2022-08-10 19:15:14 +0200

    Created UserModel, new DbSet, migration and database update

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,9 +6,10 @@ namespace dotnet_rpg.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<Character> Characters { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace dotnet_rpg.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte PasswordSalt { get; set; }
+    }
+}
