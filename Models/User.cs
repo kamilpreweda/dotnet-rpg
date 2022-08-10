287d57b86a8305a4ec0e17fc7e095dabb4dd07fa
Kamil Preweda <preweda.kamil@o2.pl>
2022-08-10 19:21:03 +0200

    Created one to many relationship between User and Characters

--- Models/User.cs
+++ Models/User.cs
@@ -6,5 +6,6 @@ namespace dotnet_rpg.Models
         public string Username { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; }
         public byte PasswordSalt { get; set; }
+        public List<Character>? Characters { get; set; }
     }
 }
