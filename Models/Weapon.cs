08980d471ebbc956738d509c30c348fa2bb4030c
Kamil Preweda <preweda.kamil@o2.pl>
2022-08-12 18:59:24 +0200

    created one-to-one relationship between characters and weapons

--- Models/Weapon.cs
+++ Models/Weapon.cs
@@ -6,5 +6,6 @@ namespace dotnet_rpg.Models
         public string Name { get; set; } = string.Empty;
         public int Damage { get; set; }
         public Character Character { get; set; }
+        public int CharacterId { get; set; }
     }
 }
