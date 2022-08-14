a16c7599706b4ebbb7cc1565582d147f70219f95
Kamil Preweda <preweda.kamil@o2.pl>
2022-08-14 23:40:02 +0200

    Added manual fight with weapons

--- Controllers/FightController.cs
+++ Controllers/FightController.cs
@@ -1,10 +1,11 @@
+using dotnet_rpg.Dtos.Fight;
 using dotnet_rpg.Services.FightService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
 {
     [ApiController]
-    [Route("[controller")]
+    [Route("[controller]")]
     public class FightController : ControllerBase
     {
         private readonly IFightService _fightService;
@@ -13,5 +14,12 @@ namespace dotnet_rpg.Controllers
         {
             _fightService = fightService;
         }
+
+        [HttpPost("Weapon")]
+        public async Task<ActionResult<ServiceResponse<AttackResultDto>>> WeaponAttack(WeaponAttackDto request)
+        {
+            return Ok(await _fightService.WeaponAttack(request));
+        }
+
     }
 }
